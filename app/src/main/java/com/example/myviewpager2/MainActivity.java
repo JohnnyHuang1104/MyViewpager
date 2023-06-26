712deda5f513605418de632f4954bfc161f6c7b3
Johnny Huang <a0900181636@gmail.com>
2023-06-26 11:29:11 +0800

    Third Commit

--- app/src/main/java/com/example/myviewpager2/MainActivity.java
+++ app/src/main/java/com/example/myviewpager2/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.myviewpager2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ViewPager2 viewPager2;
+    private ViewPageAdapter viewPageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //元件綁定
+        viewPager2 = findViewById(R.id.viewPage2);
+        //設定ViewPage的Adapter
+        viewPageAdapter = new ViewPageAdapter(this);
+        viewPager2.setAdapter(viewPageAdapter);
     }
 }
